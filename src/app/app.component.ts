3cef650bfcd768091d8a48d499afd8eaf8e69325
elena-emma <eemmanouilidi@athtech.gr>
2023-12-19 21:05:08 +0200

    wtf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MoviesComponent } from './components/movies/movies.component';
 import { FooterComponent } from './components/core/footer/footer.component';
 import { HeaderComponent } from './components/core/header/header.component';
-import { MovieDetailsComponent } from './components/movies/movies-list/movie-card/movie-details/movie-details.component';
+import { MovieDetailsComponent } from './components/movies/movie-details/movie-details.component';
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
 import { SideMenuComponent } from './components/movies/movies-list/side-menu/side-menu.component';
-import { ShopingCartComponent } from './components/movies/movies-list/shoping-cart/shoping-cart.component';
+import { ShopingCartComponent } from './components/movies/shoping-cart/shoping-cart.component';
 
 
 @Component({
